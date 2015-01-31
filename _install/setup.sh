37f35b809b8b42f7a7dea104eb7ae1eb1a61be20
Cade Scroggins <scroggins.cade@gmail.com>
2015-01-31 00:23:26 -0800

    remove newline

--- _install/setup.sh
+++ _install/setup.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+SCRIPT="${BASH_SOURCE[0]}"
+DIR="$(cd "$( dirname $SCRIPT )" && pwd)"
 INDENT="%-34s"
 BAD=0
 
 success() {
-  echo "Success!"
+  echo " Success!"
 }
 
 failure() {
-  echo "Failed!"
+  echo " Failed!"
 }
 
 please() {
-  echo "Nada, please install."
+  echo " Nada, please install."
 }
 
 checkCommand() {
@@ -32,23 +33,23 @@ cd "$DIR"
 
 # Check for commands.
 checkCommand "curl" && success || please
-checkCommand "eyeD3" && success && ln -fs `which eyeD3` ../eyeD3 || please
-checkCommand "AtomicParsley" && success && ln -fs `which AtomicParsley` ../AtomicParsley || please
-checkCommand "zip" && success && ln -fs `which zip` ../zip || please
-checkCommand "find" && success && ln -fs `which find` ../find || please
+checkCommand "eyeD3" && success && ln -fs "$(which eyeD3)" ../eyeD3 || please
+checkCommand "AtomicParsley" && success && ln -fs "$(which AtomicParsley)" ../AtomicParsley || please
+checkCommand "zip" && success && ln -fs "$(which zip)" ../zip || please
+checkCommand "find" && success && ln -fs "$(which find)" ../find || please
 checkCommand "file" && success || please
 checkCommand "php" && success || please
 
 # If they need to install stuff let them know.
 if [ $BAD -ne 0 ]; then
-  printf "\nPlease install the missing commands ($BAD) and run this script again.\n\n"
+  printf "\nPlease install the missing commands ($BAD) and run $SCRIPT\n"
   exit 1
 fi
 
 # Create folders.
 printf "$INDENT" "Creating folders..."
-mkdir -p ../{archives,artwork,songs} && echo "Success!" || echo "Failed."
+mkdir -p ../{archives,artwork,songs} && success || failure
 
 # Update folder permissions.
 printf "$INDENT" "Changing folder permissions..."
-chmod 777 ../{archives,artwork,songs} && echo "Success!" || echo "Failed."
+chmod 777 ../{archives,artwork,songs} && success || failure

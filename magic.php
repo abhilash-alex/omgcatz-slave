5a6275118458f037d5bf2cc95b2f1d6e1e088faa
Cade Scroggins <scroggins.cade@gmail.com>
2015-05-02 12:20:54 -0700

    hopefully allow big file downloads

--- magic.php
+++ magic.php
@@ -1,7 +1,5 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-
 $filePath = $_GET["p"];
 $fileName = $_GET["s"];
 
@@ -12,8 +10,23 @@ switch ($ext) {
   case "m4a":
   case "zip":
     header("Access-Control-Allow-Origin: *");
-    header("Content-Length: ".filesize($filePath));
     header("Content-Type: application/octet-stream");
+    header('Expires: 0');
+    header("Content-Length: ".filesize($filePath));
     header('Content-Disposition: attachment; filename="'.$fileName.'"');
-    readfile($filePath);
+    
+    // http://stackoverflow.com/questions/22934420/apache-php-large-file-download-2gb-failing
+
+    @ob_end_flush();
+    flush();
+
+    $fileDescriptor = fopen($filePath, 'rb');
+
+    while ($chunk = fread($fileDescriptor, 8192)) {
+      echo $chunk;
+      @ob_end_flush();
+      flush();
+    }
+
+    fclose($fileDescriptor);
 }
